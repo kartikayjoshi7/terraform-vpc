c3e82b6c0849294619c1d645f4fc1cb26dfd0e05
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-25 04:52:20 +0530

    File updated

--- subnets.tf
+++ subnets.tf
@@ -2,17 +2,21 @@ resource "aws_subnet" "public-subnets" {
   count = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PUBLIC_SUBNET_CIDR[count.index]
+  availability_zone = var.AZS[count.index]
 
   tags = {
-    Name = "public-subnet-${count.index + 1}"
+    Name = "${var.ENV}.public-subnet-${count.index + 1}"
+    ENV=var.ENV
   }
 }
 resource "aws_subnet" "private-subnets" {
   count = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PRIVATE_SUBNET_CIDR[count.index]
+  availability_zone = var.AZS[count.index]
 
   tags = {
-    Name = "private-subnet-${count.index + 1}"
+    Name = "${var.ENV}.private-subnet-${count.index + 1}"
+    ENV=var.ENV
   }
 }
