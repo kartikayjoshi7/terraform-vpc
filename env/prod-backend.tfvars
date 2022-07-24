06f7e7f97b465fbc9869d7d4368b61d2fc01bf2a
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-25 03:49:07 +0530

    File updated

---
+++ env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-kartikayj"
+key    = "vpc/prod/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.VPC_CIDR
+
+
+  tags = {
+    Name = var.ENV
+  }
+}
