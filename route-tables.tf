45409df137f473f2ad9494f87efe2bacfa2c1639
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-30 19:58:35 +0530

    File updated

--- route-tables.tf
+++ route-tables.tf
@@ -63,6 +63,14 @@ resource "aws_route" "private-peer-route" {
 
 }
 
+resource "aws_route" "default-rtable-route" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+
+}
+
+
 
 
 
